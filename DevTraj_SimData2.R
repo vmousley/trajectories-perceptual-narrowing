f52f371244167e2f0faef3887b5172fabba14119
vmousley <v.mousley.17@ucl.ac.uk>
2019-06-17 14:14:41 +0100

    original file sent to Dev Sci, March 2019

---
+++ DevTraj_SimData2.R
@@ -0,0 +1,152 @@
+# "Developmental trajectories of perceptual narrowing among monolingual and bilingual infants"
+# Stage 1: Registered Report
+# XXXXXXXXXXXXXXXXXXXXX
+# Submitted to Developmental Science
+
+setwd("~/trajectories-perceptual-narrowing")
+mydata <- data.frame(read.csv("SimulationData25Feb.csv"))
+require("plyr")
+require("dplyr")
+
+#### Data Cleaning ####
+
+# Rename columns
+mydata <- plyr::rename(mydata, c("Participant.ID" = "ID",
+                                 "Language..0...mono..1...biling." = "Group",
+                                 "Age..days." = "Age",
+                                 "X..habit.trials" = "NoHabitTrials",
+                                 "Mean.habit.LT..sec." = "MeanHabit", 
+                                 "Pre.test.LT..sec." = "PreTest",
+                                 "Same.cond.LT..sec." = "Same",
+                                 "Switch.cond.LT..sec." = "Switch",
+                                 "Post.test.LT..sec." = "PostTest",
+                                 "Total.sequence.LT..sec." = "TotalLt",
+                                 "Diff.score.LT..sec." = "DiffScore")) 
+
+# Erase extra rows (added by R)
+mydata <- distinct(mydata, .keep_all = FALSE); mydata
+mydata <- mydata[-c(73), ]; mydata
+
+# Numeric to Categorical (Group)
+mydata$Group <- as.factor(mydata$Group)
+mydata$Group <- revalue(mydata$Group, 
+                        c("0"="monolingual", "1"="bilingual"))
+
+# Define outcome: total looking time to test phase
+mydata$TotalTest <- cbind(mydata$Same + mydata$Switch)
+
+#### Graphical Analysis ####
+
+# Age x Difference Score
+plotAgexDiff <- plot(mydata$Age, mydata$TotalTest, 
+                     xlab="Age (days)", 
+                     ylab="Total LT to Test", 
+                     cex=1, pch="X", 
+                     cex.axis=1, 
+                     cex.lab=1, col='blue')
+
+# Pairs: Difference Score x Age
+pairs(mydata)
+pairs(~TotalTest+Age, data=mydata)
+
+# Plot: Difference Score and Group
+plot(mydata$TotalTest, mydata$Group)
+
+# Language Group x Difference Score (****** ISSUE WITH MEANS ******)
+groupscatter <- plot(as.numeric(mydata$Group), mydata$DiffScore,
+                     ylab="Difference Score (Same - Switch)", xaxt="n",
+                     xlab="Language Group",
+                     cex=1.5, pch="*", 
+                     cex.axis=1, 
+                     cex.lab=1, col='purple')
+axis(1,1:2, labels=levels(mydata$Group), cex.axis=1)
+groupscatter <- points(c(1,2), 
+                       c(mean(mydata$DiffScore[mydata$Group=='monolingual']), 
+                         mean(mydata$DiffScore[mydata$Group=='bilingual'])), pch="---", cex=4)
+
+
+# Difference score distribution
+hist <- hist(mydata$DiffScore, 
+             xlab = "Difference Score (Same - Switch)", 
+             ylab = "Frequency")
+
+#### Basic Functions ####
+
+# Descriptive Statistics
+descriptives <- summary(mydata)
+dim(mydata)
+names(mydata)
+
+# Corr between Age and Difference Score
+cor(mydata$TotalTest, mydata$Age, method="pearson")
+
+# Associations between Difference Score ~ Group
+tapply(mydata$TotalTest, mydata$Group, summary)
+t.test(TotalTest ~ Group, data=mydata)
+summary(aov(TotalTest ~ Group, data=mydata))
+wilcox.test(TotalTest ~ Group, data=mydata)
+kruskal.test(TotalTest ~ Group, data=mydata)
+
+#### Linear Modelling ####
+
+## Note to self: ASSUMPTIONS! 
+# All observations independent
+# y is linearly associated with predictors
+# errors are normally distributed around 0 with constant variance across all predicted y values
+
+m0 <- lm(TotalTest ~ 1, data=mydata)
+m1 <- lm(TotalTest ~ Age*Group*Switch, data=mydata)
+summary(m1)
+
+names(summary(m1))
+
+# Isolate coefficients and CIs 
+coefficients(m1)
+confint(m1)
+
+# Goodness of Fit
+summary(m1)$adj.r.squared
+deviance(m1)
+BIC(m1)
+
+# Explore fitted values, predictions, and residuals
+fit.m <- fitted(m1); summary(fit.m)
+predict.m <- predict(m1); summary(predict.m)
+
+plot(as.numeric(mydata$Group), mydata$TotalTest, 
+     ylab = "Total Test LT",
+     xlab = "Language Group", 
+     cex=1, pch="*", xaxt="n", col="blue", cex.axis=1, 
+     cex.lab=1)
+axis(1, 1:2, labels=c("Monolingual", "Bilingual"), cex.axis=1)
+points(mydata$Group, fit.m, pch="---", cex=2)
+
+residuals(m1)
+rstandard(m1)
+rstudent(m1)
+
+# Explore residuals
+hist(rstandard(m1))
+hist(dffits(m1))
+hist(dfbetas(m1)[,"Age"], nclass=10)
+# tells you how much the outcome will change if you take out that predictor (?double check)
+hist(cooks.distance(m1),nclass=10)
+
+# Influence measures
+inf.m <- influence.measures(m1)
+par(mfrow=c(3,3))
+for(i in 1:(dim(inf.m$infmat)[2]-1)){
+  hist(inf.m$infmat[,i],
+       main=dimnames(inf.m$infmat)[[2]][i])
+}
+
+# Identifying outliers by ID number
+mydata$ID[cooks.distance(m1) > 0.2]
+
+# Plot of model/residuals
+layout(matrix(c(1,2,3,4),2,2))
+plot(m1)
+
+# Collinearity measures 
+require(car)
+vif(m1)
